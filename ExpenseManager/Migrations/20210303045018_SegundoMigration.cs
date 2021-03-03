83f22517e16298a929a5c47147cd22d1a196190a
Marcos Moraes <marcos.moraes.ext@tivit.com>
2021-03-03 02:01:54 -0300

    Adicionar arquivos de projeto.

---
+++ ExpenseManager/Migrations/20210303045018_SegundoMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpenseManager.Migrations
+{
+    public partial class SegundoMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
